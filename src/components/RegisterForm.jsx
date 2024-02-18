f026148913d6a9c36577e88cf9338068c2ca450f
Neftaly <neftalyrv23@gmail.com>
2024-02-18 16:21:00 -0400

    Made logic to display errors without transitions

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react"
+import Error from "./Error"
+import Input from "./Input"
+export default function RegisterForm() {
+    
+    const [errors, setErrors] = useState(null)
+    
+    // Run this when errors change
+    useEffect(() => {
+        console.log("Some errors occurred")
+        console.log(errors)
+    },[errors])
+
+    function registerHandler(e) {
+        e.preventDefault()
+        const data = Object.fromEntries(
+            new FormData(e.target)
+        )
+        register(data)
+    }
+
+    async function register(data) {
+        try {
+            const request = await fetch(
+            'http://localhost:3000/api/session/register',
+            {
+                method: 'post',
+                headers: {
+                    "Content-Type" : "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+
+            const response = await request.json()
+            
+            if(request.status === 422) {
+                setErrors(response.errors)
+            }
+        } catch(e) {
+            console.log("Catching errors")
+            console.log(e)
+        }
+        
+    }
+
+    return (
+        <form onSubmit={registerHandler}>
+            <div className="group">
+                <Input
+                    type={'text'}
+                    placeholder={'Username'}
+                    name={"username"} /> 
+                <Error name={'username'} errors={errors} />
+            </div>
+            <div className="group">
+                <Input
+                    type={'email'}
+                    placeholder={'E-mail'}
+                    name={"email"} /> 
+                <Error name={'email'} errors={errors}/>
+            </div>
+            <div className="group">
+                <Input
+                    type={'password'}
+                    placeholder={'Password'}
+                    name={"password"} />
+                <Error name={'password'} errors={errors} />
+            </div>
+            <div className="group">
+                <Input
+                    type={'password'}
+                    placeholder={'Confirm password'}
+                    name={"confirmPassword"} />
+                <Error name={'confirmPassword'} errors={errors} />
+            </div>
+            <button type="submit">register</button>
+        </form>
+    )
+}
