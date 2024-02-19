5e2cbae404503fa7b7715f13400314b472427b46
Neftaly <neftalyrv23@gmail.com>
2024-02-19 14:09:26 -0400

    Register page component with some styles

---
+++ src/pages/register/RegisterPage.jsx
@@ -0,0 +1,10 @@
+import RegisterForm from "../../components/register/RegisterForm";
+
+export default function RegisterPage() {
+    return(
+        <div className="flex full-height full-width">
+            <span>Logo</span>
+            <RegisterForm />
+        </div>
+    )
+}
