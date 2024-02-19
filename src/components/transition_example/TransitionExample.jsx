30aabe4dbcd2cddf3d026957c12214ccf8e51b3c
Neftaly <neftalyrv23@gmail.com>
2024-02-19 14:09:49 -0400

    Example of how the transition library works

---
+++ src/components/transition_example/TransitionExample.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { CSSTransition } from "react-transition-group";
+import './example.css'
+// Example of how the transition dependency works
+export default function TransitionExample() {
+
+    const [isActive, setActive] = useState(false)
+    
+    const applyTransition = () => {
+        console.log(isActive)
+        if(!isActive) {
+            setActive(true)
+            return
+        }
+        setActive(false)
+        
+    }
+
+    return (
+        <>
+            <button onClick={applyTransition}>Apply transition</button>
+            <CSSTransition 
+                in={isActive}
+                classNames='error-transition'
+                timeout={2000}
+                
+                >
+                <h1 className="example">{isActive ? "isActive" : "notActive"}</h1>
+            </CSSTransition>
+            
+        </>
+        
+    )
+}
