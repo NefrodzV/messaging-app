63beefd5c2d39711b6eab8039a105733cf8a1a44
Neftaly <neftalyrv23@gmail.com>
2024-02-19 14:08:30 -0400

    made input and style its outline

---
+++ src/components/input/Input.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import './input.css'
+export default function Input({type, name, id, label, placeholder}) {
+    return(
+        <>
+            <label htmlFor={id}>{label}</label>
+            <input 
+                className='primary-outline'
+                type={type} 
+                placeholder={placeholder} 
+                name={name} 
+                id={id} 
+            />
+        </>
+    )
+}
+
+Input.propTypes = {
+    type: PropTypes.string,
+    name: PropTypes.string,
+    id: PropTypes.string,
+    label: PropTypes.string,
+    placeholder: PropTypes.string,
+    isRequired: PropTypes.bool
+}
