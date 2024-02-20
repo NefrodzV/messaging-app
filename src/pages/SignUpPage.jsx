7804bd75c79e8d291c5d59a8a470a95f723c5167
Neftaly <neftalyrv23@gmail.com>
2024-02-20 13:03:10 -0400

    refactored to use css modle

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -3,7 +3,7 @@ import signUpStyles from '../stylesheets/sign-up.module.css'
 
 export default function SignUpPage() {
     return(
-        <div className="signup-page full-height full-width">
+        <div className={signUpStyles.page}>
             <div className="brand">LOGO</div>
             <SignUpForm />
         </div>
