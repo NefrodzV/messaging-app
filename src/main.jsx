2a8054cb5f1c6a355870a9119d9a25c95bc67b07
Neftaly <neftalyrv23@gmail.com>
2024-02-22 16:14:04 -0400

    Changed how to init the app to set up with global state

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import router from './Router.jsx'
-import { RouterProvider } from 'react-router-dom'
+
 import './stylesheets/styles.css'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 )
