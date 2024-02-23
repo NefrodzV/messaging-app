67d0c2666c18057a929cc731cefa012f3c2c8a55
Neftaly <neftalyrv23@gmail.com>
2024-02-23 19:28:19 -0400

    Made check if the login page was navigated by a pop and reset token

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,19 @@
 import loginStyles from '../stylesheets/login.module.css'
 import LoginForm from '../components/LoginForm'
+import { useContext, useEffect } from 'react'
+import { UserContext } from '../contexts/UserContext'
+import { useNavigationType } from 'react-router-dom'
 
 export default function LoginPage() {
+    const navType = useNavigationType()
+    const { resetToken } = useContext(UserContext)
+
+    useEffect(() => {
+        if(navType === "POP") {
+            resetToken()
+        }
+    },[navType, resetToken])
+
     return (
         <div className={loginStyles.page}>
             <div className='brand'>Logo</div>
