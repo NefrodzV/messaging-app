79d7726f60ddab45d7c70ae8c7c96455b7b4bfcb
Neftaly <neftalyrv23@gmail.com>
2024-02-23 19:34:53 -0400

    Set up global state for user and a function to reset the token when needed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { RouterProvider } from 'react-router-dom'
 import router from './Router'
-import  UserContext  from './contexts/UserContext'
+import  { UserContext }  from './contexts/UserContext'
 import { useState } from 'react'
 
 function App() {
-  
-  const [token, setToken] = useState(null)
 
-  // const updateToken = (data) => {
-  //   setToken(data)
-  // }
-  
+  const [token, setToken] = useState(null)
+  const resetToken = () => {
+
+    setToken(null)
+  }
+
   return (
     <>
-      <UserContext.Provider value={{token: token, updateToken:setToken}} >
+      <UserContext.Provider value={{
+        token, 
+        setToken,
+        resetToken }} >
         <RouterProvider router={router} />
       </UserContext.Provider>
     </>
