4a09a31d45fa82a41be76661b9e1b7833a5e9ebc
Neftaly <neftalyrv23@gmail.com>
2024-02-24 22:11:02 -0400

    Set up cookies  when user logs in and updated state if there is an existing one

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import { RouterProvider } from 'react-router-dom'
 import router from './Router'
 import  { UserContext }  from './contexts/UserContext'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Cookies from 'js-cookie'
 
 function App() {
 
   const [token, setToken] = useState(null)
-  const resetToken = () => {
 
+  const resetToken = () => {
     setToken(null)
   }
 
+  useEffect(() => {
+    const data = Cookies.get("token")
+    if(data && data !== "null") {
+      console.log("cookie:" + data)
+      setToken(data)
+    }
+  })
+
+  useEffect(() => {
+    if(token) {
+      Cookies.set('token', token, { expires: 3 })
+    }
+  },[token])
   return (
     <>
       <UserContext.Provider value={{
