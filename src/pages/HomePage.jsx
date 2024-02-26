6a84732a14db817ddd911946e7a2572fefdabc2d
Neftaly <neftalyrv23@gmail.com>
2024-02-26 15:40:16 -0400

    change login state to a bool and added some elements with some styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,16 +2,18 @@ import { useContext, useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { UserContext } from "../contexts/UserContext"
 import Header from "../components/Header"
+import homeStyle from '../stylesheets/homepage.module.css'
+import ChatList from "../components/ChatList"
 
 export default function HomePage() {
 
-    const [ loading, setLoading] = useState(true)
-    const { token } = useContext(UserContext)
+    const [ loading, setLoading ] = useState(true)
+    const { isLoggedIn } = useContext(UserContext)
     
     const navigate = useNavigate()
 
     useEffect(() => {
-        if(!token) {
+        if(!isLoggedIn) {
             navigate('/login')
             return
         }
@@ -21,8 +23,14 @@ export default function HomePage() {
     return (
         <>
             { loading ? <h1>Loading...</h1> : 
-                <div className="wrapper">
+                <div className={homeStyle.wrapper}>
                     <Header />
+                    <main>
+                        <aside>
+                            My side bar
+                        </aside>
+                        <ChatList />
+                    </main>
                 </div> 
             }
         </>
