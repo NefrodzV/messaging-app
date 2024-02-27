baa87e5c044c878194fbd18352ebb361db5d4598
Neftaly <neftalyrv23@gmail.com>
2024-02-27 14:59:07 -0400

    Change name

---
+++ src/components/ChatCard.jsx
@@ -0,0 +1,10 @@
+
+import chatStyle from '../stylesheets/chat.module.css'
+export default function ChatCard() {
+    return (
+        <li className={chatStyle.chat}>
+            <div className={chatStyle.date}>09/Jan/2024</div>
+            <div className={chatStyle.message}>Epso Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis accusamus maiores unde corporis provident exercitationem maxime dolorem. Error velit, odit a numquam nisi ab rerum, architecto aliquam, reiciendis inventore molestiae.</div>
+        </li>
+    )
+}
