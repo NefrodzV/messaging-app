7c27b6c04ac46ac22cd49cab8b1a9ac1b93b41d6
Neftaly <neftalyrv23@gmail.com>
2024-02-29 12:46:31 -0400

    Added accessibility to card element

--- src/components/ChatCard.jsx
+++ src/components/ChatCard.jsx
@@ -2,10 +2,13 @@ import { Link } from 'react-router-dom'
 import style from '../stylesheets/chatcard.module.css'
 export default function ChatCard({ chat }) {
     return (
-        <li className={style.chat}>
+        <li className={style.chat}> 
+            <Link className={style.link} to={`/chat/${chat._id}`}>
+                Go to chat with user
+            </Link>
             <h3 className={ style.date }>{chat.lastMessage.date}</h3>
             <h4 className={ style.message }>{chat.lastMessage.text}</h4>
-            <Link to={`/chat/${chat._id}`}>Go to chat with</Link>
+           
         </li>
     )
 }
