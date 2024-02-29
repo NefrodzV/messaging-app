9bd6acba654f8a54e464456ad44d70ffee4b584c
Neftaly <neftalyrv23@gmail.com>
2024-02-29 15:29:43 -0400

    refactor some code for names

--- src/components/MessageCard.jsx
+++ src/components/MessageCard.jsx
@@ -1,9 +1,8 @@
 import style from '../stylesheets/messagecard.module.css'
-import icon from '../assets/user.svg'
 export default function MessageCard({ message }) {
     return (
-        <li className={style.message}>
-            <img className={style.pic} src={icon}/> 
+        <li className={style.card}>
+            {/* <img className={style.pic} src={icon}/>  */}
             <div>
                 <h3>{message.date}</h3>
                 <p>{message.text}</p>
