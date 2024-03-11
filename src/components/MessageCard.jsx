3617d9c7b5b546d618b677e1e20f989d955ca6d6
Neftaly <neftalyrv23@gmail.com>
2024-03-11 17:53:34 -0400

    Set up function to format date

--- src/components/MessageCard.jsx
+++ src/components/MessageCard.jsx
@@ -1,10 +1,26 @@
 import style from '../stylesheets/messagecard.module.css'
 export default function MessageCard({ message }) {
+    function formatDate(data) {
+        const msgDate = new Date(data)
+        const today = new Date()
+        const isSameDay = msgDate.toDateString() === today.toDateString()
+        const formattedTime = msgDate.toLocaleTimeString('en-us', {
+            hour: '2-digit',
+            minute: '2-digit'
+        })
+        if(isSameDay) {
+            return "today at "+ formattedTime
+        } 
+        const formattedDate =  msgDate.toLocaleDateString('en-Us')
+        return formattedDate + " " + formattedTime
+    }
     return (
         <li className={style.card}>
+            {/*If you implement profile pics for group chats you need a 
+            condition to show it */}
             {/* <img className={style.pic} src={icon}/>  */}
             <div>
-                <h3>{message.date}</h3>
+                <h3>{formatDate(message.date)}</h3>
                 <p>{message.text}</p>
             </div>
         </li>
