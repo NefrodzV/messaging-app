b4076ec54c28a59cd3d0cca39c80fd3ce43d02c5
Neftaly <neftalyrv23@gmail.com>
2024-03-12 19:52:08 -0400

    Implemented the navigation componenent and removed logic that belongs there

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,10 @@
-import { useContext } from 'react'
-import { UserContext } from '../contexts/UserContext'
-import logout from '../assets/logout.svg'
 import headerStyles from '../stylesheets/header.module.css'
-import Cookie from 'js-cookie'
+import Navigation from './Navigation'
 export default function Header() {
-    
-    const { setIsLoggedIn } = useContext(UserContext)
-    
-    function logoutHandler() {
-        Cookie.remove("token")
-        setIsLoggedIn(false)
-    }
-   
     return(
         <header className={headerStyles.wrapper}>
             <span>Logo</span>
-            <nav>
-                <ul className={headerStyles.navlist}>
-                    <li className={headerStyles.navitem}>
-                        <button className={headerStyles.button}
-                            onClick={logoutHandler}>
-                            <img className={headerStyles.icon} src={logout}/>
-                            Log out
-                        </button>
-                    </li>
-                </ul>
-            </nav>
+           <Navigation />
         </header>
     )
 }
