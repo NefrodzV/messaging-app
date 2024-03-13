6bd639745b23b492d7f86ab106420e6940b329f5
Neftaly <neftalyrv23@gmail.com>
2024-03-12 21:56:26 -0400

    Changed code for the refactor of the useChat hook

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -9,44 +9,11 @@ import ChatHeader from './ChatHeader'
 import useChat from '../hooks/useChat'
 export default function Chat() {
 
-    const params = useParams()
-
+    const { id } = useParams()
+    
     const { token } = useContext(UserContext)
-    const { data, status } = useChat(params.id)
-    const [ messages, setMessages ] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [otherUser, setOtherUser] = useState(null)
-
-    // Gets messages from api
-    // const { data, status, refetch } = useQuery({
-    //     queryKey:['messages'],
-    //     queryFn: async () => {
-    //         const response = await fetch(
-    //            `http://localhost:3000/api/messages?chatId=${params.id}`, {
-    //             headers: {
-    //                 'authorization': "Bearer " + token
-    //             }
-    //         })
-    //         return response.json()
-    //     }
-    // })
-
-    useEffect(() => {
-        if(status === "pending") {
-            setLoading(true)
-            return
-        }
-        if(status === "success") { 
-            console.log("data in use effect")
-            console.log(data)
-            setLoading(false)
-            setMessages(data.messages)
-            setOtherUser(data.chat.users[0])
-           
-        }
-    },[status,data])
-
-
+    const { data, loading, error } = useChat(id)
+    
     // Sends message to api
     const messageMutation = useMutation({
         mutationFn: sendMessage,
@@ -82,30 +49,24 @@ export default function Chat() {
         e.target.reset()
     }
 
+    if(loading) return <div>Loading...</div> 
+    if(error) return <div>Whoops something went wrong</div>
     
-
-    console.log(params.id)
     return(
-        <>
+        <div className={chatStyle.chat}>
+            <ChatHeader user={data.chat.users[0]} />
+            <ul className={chatStyle.messagelist}>     
             {
-                loading ? <div>Loading...</div> :
-                <div className={chatStyle.chat}>
-                    <ChatHeader user={otherUser} />
-                    <ul className={chatStyle.messagelist}>     
-                    {
-                        messages?.map((message) => 
-                            <MessageCard key={message._id} message={message} />)
-                    }
-                    </ul>
-                    <form noValidate={true} onSubmit={sendMessageHandler}>
-                        <div className={chatStyle.control}>
-                            <input type="text" name="message" id="message" />
-                            <button>send</button>
-                        </div>
-                    </form>
-                </div>
+                data.messages?.map((message) => 
+                    <MessageCard key={message._id} message={message} />)
             }
-        </>
-        
+            </ul>
+            <form noValidate={true} onSubmit={sendMessageHandler}>
+                <div className={chatStyle.control}>
+                    <input type="text" name="message" id="message" />
+                    <button>send</button>
+                </div>
+            </form>
+        </div>  
     )
 }
