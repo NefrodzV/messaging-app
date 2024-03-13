c9d9158f431be1ee602a39d1c0855d51837cd211
Neftaly <neftalyrv23@gmail.com>
2024-03-13 13:11:34 -0400

    Changed useQuery based logic to promise based ones

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,42 +1,58 @@
 import { useParams } from 'react-router-dom'
-import { useState } from 'react'
-import { useMutation, useQuery  } from '@tanstack/react-query'
+import { useMutation } from '@tanstack/react-query'
 import chatStyle from '../stylesheets/chat.module.css'
-import { useContext, useEffect } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import {UserContext} from '../contexts/UserContext'
 import MessageCard from './MessageCard'
 import ChatHeader from './ChatHeader'
 import useChat from '../hooks/useChat'
+
 export default function Chat() {
 
     const { id } = useParams()
-    
     const { token } = useContext(UserContext)
-    const { data, loading, error } = useChat(id)
-    
-    // Sends message to api
-    const messageMutation = useMutation({
-        mutationFn: sendMessage,
-        onSettled: (data, error, variables, context)  => {
-            refetch()
-        }
-    })
+    const { data, loading, error, update } = useChat(id)
+    // TODO: MOVE ALL LOGIC FOR SENDING A MESSAGE TO ITS OWN CUSTOM HOOK
+    // loading state when sending a message
+    const [sentMessageIsLoading, setSentMessageIsLoading] = useState(false)
+    // data of the response of the message sent
+    const [sentMessage, setSentMessage] =  useState(null)
+    // errors of the response when sending the message
+    const [sentMessageError, setSentMessageError] = useState(null)
 
-    function sendMessage(message) {
-        const body = {
-            message: message
-        }
-        return fetch(
-            `http://localhost:3000/api/messages?chatId=${params.id}`,
-            {
-                method: "POST",
-                headers:{
-                    "authorization": "Bearer " + token,
-                    "content-type": "application/json"
-                },
-                body: JSON.stringify(body)
+    useEffect(() => {
+        update()
+    },[sentMessage, update])
+
+    // Sends message to api
+    async function sendMessage(message) {
+        try {
+            setSentMessageIsLoading(true)
+            const body = {
+                message: message
             }
-        )
+            const response = await fetch(
+                `http://localhost:3000/api/messages?chatId=${id}`,
+                {
+                    method: "POST",
+                    headers:{
+                        "authorization": "Bearer " + token,
+                        "content-type": "application/json"
+                    },
+                    body: JSON.stringify(body)
+                }
+            )
+            const json = await response.json()
+            if(!response.ok) {
+                setSentMessageError(json.errors)
+                return
+            }
+            setSentMessage(json)
+        } catch(e) {
+            setSentMessageError("Some error happened sending message:" + e)
+        } finally {
+            setSentMessageIsLoading(false)
+        }
     }
 
     function sendMessageHandler(e) {
@@ -44,8 +60,9 @@ export default function Chat() {
         const message = e.target["message"].value
         if(!message.trim()) {
             return
-        } 
-        messageMutation.mutate(message)
+        }
+        sendMessage(message)
+        // messageMutation.mutate(message)
         e.target.reset()
     }
 
