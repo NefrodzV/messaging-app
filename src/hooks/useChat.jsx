4dd682028bb2f6f1908afebad76acc7432911afe
Neftaly <neftalyrv23@gmail.com>
2024-03-13 13:11:54 -0400

    Implemented a function to be able to update the chat externally

--- src/hooks/useChat.jsx
+++ src/hooks/useChat.jsx
@@ -3,11 +3,12 @@ import { useContext, useEffect, useState } from "react"
 // Returns a specific chat with id
 export default function useChat(id) {
 
+    
     const { token } = useContext(UserContext)
     const [data, setData] = useState(null)
     const [loading, setLoading] = useState(true)
     // This is not in use right now  but in the future it might
-    // const [errors, setErrors] = useState({})
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         getChat()
@@ -34,11 +35,17 @@ export default function useChat(id) {
             }
             setData(json)
         } catch(e) {
-            // setStatus('error')
+            setError("Error getting chat data: " + e)
             throw new Error("Error fetching chat data: " + e)
         } finally  {
             setLoading(false)
         }
     }
-    return { data, loading }
+
+    function update() {
+        getChat()
+    }
+
+    // TODO: Implement a reload function here
+    return { data, loading, error, update }
 }
