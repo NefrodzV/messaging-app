d76f9423c17ce651d50f05749318f639a449b1ab
Neftaly <neftalyrv23@gmail.com>
2024-03-26 14:35:41 -0400

    Made logic to handle the images from the user data

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,13 +1,21 @@
 import PropTypes from 'prop-types'
 import style from '../stylesheets/usercard.module.css'
-import exampleImg from '../assets/logout.svg'
 import { Link } from 'react-router-dom'
+import userIcon from '../assets/user.svg'
 export default function UserCard({ user }) {
+    function imageHandler(image) {
+        const url = `data:${image.mimeType};base64,${image.data}`
+        return url
+    }
     return (
         <div className={style.card}>
-            <img className={style.profile} src={exampleImg} />
-            <p className={style.text}>{user.profile.username}</p>
-            <Link to={`/chat?userId=${user._id}&username=${user.profile.username}`}>Chat</Link>
+            <img className={style.profile} 
+                src={
+                    user?.profile?.image ? imageHandler(user?.profile.image) :
+                    userIcon
+                } />
+            <p className={style.text}>{user?.profile?.username}</p>
+            <Link to={`/chat?userId=${user._id}&username=${user?.profile?.username}`}>Chat</Link>
         </div>
     )
 }
