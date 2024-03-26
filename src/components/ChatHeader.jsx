05ee207e0aceac519126639b5d383bfad452001b
Neftaly <neftalyrv23@gmail.com>
2024-03-26 11:30:40 -0400

    Implemented a style for loading text

--- src/components/ChatHeader.jsx
+++ src/components/ChatHeader.jsx
@@ -8,15 +8,24 @@ export default function ChatHeader({ user }) {
         const url = `data:${image.mimeType};base64,${image.data}`
         return url
     }
-
+    
     return (
         <div className={style.header} >
             <img 
                 src={
-                    user.profile?.image ? imageHandler(chat.user.image) : userIcon
+                    user?.profile?.image ? imageHandler(chat.user.image) : userIcon
                 }
                 alt="Image of user I am chatting with" />
-            <h2>{user.profile?.username}</h2>
+            {user ? <h2>{user?.profile?.username}</h2> :
+                <>
+                    <h2 style={{
+                        backgroundColor: 'lightgray',
+                        width:'25%',
+                        height: '100%'
+                    }}></h2>
+                </>
+            }
+            
         </div>
     )
 }
