44a9c51392a18c3ce3b3fa1b1f25a0fb198fe5e9
Neftaly <neftalyrv23@gmail.com>
2024-03-28 18:59:39 -0400

    Created functions to handle chat existence and creation. If id is updated the just get chat data. If a user has updated and there is no id for specific chat create chat.

--- src/components/CreateChat.jsx
+++ src/components/CreateChat.jsx
@@ -1,15 +1,23 @@
-import { useNavigate, useSearchParams } from "react-router-dom"
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom"
 import chatStyle from '../stylesheets/chat.module.css'
 import { useContext, useEffect, useState } from "react"
 import { UserContext } from "../contexts/UserContext"
+import Loader from './Loader'
+import ChatHeader from './ChatHeader'
 
 export default function CreateChat() {
+    const  { state: { user } } = useLocation()
+    console.log(user)
     const { token } = useContext(UserContext)
     const navigate = useNavigate()
 
     const [params] = useSearchParams()
     const [loading, setLoading] = useState(true)
+    //Other user that will be sent the message or start the chat with
+    
+    useEffect(() => {
 
+    })
     async function createChat(message) {
         const body = {
             userId: params.get("userId"),
@@ -51,15 +59,40 @@ export default function CreateChat() {
         const data = await response.json()
 
         if(!response.ok) {
-            setLoading(false)
+            // If there inst an already made chat get the selected user with id
+            getUserWithId()
             return
         }
         navigate('/chat/' + data.chat._id)
 
     }
-    useEffect(() => {
-        checkChatExistence()
-    })
+
+    async function getUserWithId() {
+        try {
+            const userId = params.get('userId')
+            const response = 
+            await fetch(`http://localhost:3000/api/chats?userId=${userId}`,
+            {
+                headers: {
+                    "authorization": "Bearer " + token
+                }
+            })
+
+            const json = await response.json() 
+            if(!response.ok) {
+                console.log("Something went wrong looking for user with id" + json.errors)
+                return
+            } 
+            setUser(json.user)
+        } catch(e) {
+            console.log("error happened getting user with id" + e)
+        } finally {
+            setLoading(false)
+        }
+    }
+    // useEffect(() => {
+    //     checkChatExistence()
+    // },[token])
     function createChatHandler(e) {
         e.preventDefault()
         const message = e.target["message"].value
@@ -67,24 +100,26 @@ export default function CreateChat() {
     }
 
     return(
-        <>
-            {
-                loading ? <div>Loading ...</div> :
-                <div className={chatStyle.chat}>
-                <h2>Start chat with user</h2>
+    
+        <div className={chatStyle.chat}>
+        <ChatHeader 
+            username={user?.profile?.username}  
+            image={user?.profile?.image}/>
+            { loading ? <Loader /> : 
+                <>
                     <ul className={chatStyle.messagelist}>
-                        
+                        Send a message to start a chat
                     </ul>
                     <form noValidate={true} onSubmit={createChatHandler}>
                         <div className={chatStyle.control}>
-                            <input type="text" name="message" id="message" 
+                            <input 
+                            type="text" name="message" id="message" 
                             placeholder="Send message to start a chat with this user..."/>
                             <button>send</button>
                         </div>
                     </form>
-               </div>
+                </>
             }
-        </>
-        
+        </div>
     )
 }
--- src/hooks/useChat.jsx
+++ src/hooks/useChat.jsx
@@ -1,18 +1,37 @@
+import { useNavigate } from "react-router-dom"
 import { UserContext } from "../contexts/UserContext"
 import { useCallback, useContext, useEffect, useState } from "react"
 // Returns a specific chat with id
-export default function useChat(id) {
+export default function useChat(id, user) {
 
+    const navigate = useNavigate()
     const { token } = useContext(UserContext)
     const [data, setData] = useState(null)
     const [loading, setLoading] = useState(true)
     // This is not in use right now  but in the future it might
     const [error, setError] = useState(null)
-    const handleGetChat = useCallback(getChat, [id, token])
-    const updateHandler = useCallback(update,[id])
+    const getChatHandler = useCallback(getChat, [id, token])
+    const updateHandler = useCallback(update,[getChatHandler])
+    const existenceHandler = useCallback(checkChatExistence,[user,navigate,token])
+    const createHandler = useCallback(createChat,[navigate, user, token])
     useEffect(() => {
-        handleGetChat()
-    },[id, handleGetChat])
+        // No need to get this if data was return for checking with user id
+        if(data) return
+        if(!id) {
+            
+            return
+        }
+        
+        getChatHandler()
+    },[id, data, getChatHandler])
+
+    // For checking chat existence
+    useEffect(() => {
+        // If the chat is already identified return no need to check
+        if(id) return
+        if(!user) return
+        existenceHandler()
+    },[id,user, existenceHandler])
 
     async function getChat() {
         try {
@@ -28,7 +47,7 @@ export default function useChat(id) {
             if(!response.ok) {
                 // setStatus('error')
                 // setErrors(data.errors)
-                console.log(data.errors)
+                console.log(json.errors)
                 return
             }
             setData(json)
@@ -41,9 +60,61 @@ export default function useChat(id) {
     }
 
     function update() {
-        getChat()
+        getChatHandler()
+    }
+
+    async function checkChatExistence() {
+        try {
+            const response = await fetch(
+            `http://localhost:3000/api/chats?userId=${user._id}`,
+            {
+                headers: {
+                    "authorization": "Bearer " + token
+                }
+            })
+
+            const data = await response.json()
+
+            if(!response.ok) {
+                // If there inst an already made chat get the selected user with id
+                return
+            }
+            navigate('/chats/' + data.chat._id)
+        } catch(e) {
+            setError('Some error happened' + e)
+            throw new Error("Something went wrong checking chat existence: " + e)
+        } finally {
+            setLoading(false)
+        }
+
+    }
+    async function createChat(message) {
+        console.log(user)
+        const body = {
+            userId:user._id,
+            message: message
+        }
+        const response = await fetch('http://localhost:3000/api/chats',{
+            method: 'POST',
+            headers:{
+                "authorization": "Bearer " + token,
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(body)
+        })
+
+        const json = await response.json()
+
+        if(!response.ok) {
+            throw new Error('Something went wrong with sending message...')
+        }
+
+        setData(json)
+
+        // Redirect to chat component
+        navigate('/chats/' + json.chatId)
     }
 
     // TODO: Implement a reload function here
-    return { data, loading, error, update: updateHandler }
+    return { data, loading, error, update: updateHandler, create: createHandler }
 }
