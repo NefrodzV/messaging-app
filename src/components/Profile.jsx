95e7b611101a4681b76c6d6dbb48fadad526cbdd
Neftaly <neftalyrv23@gmail.com>
2024-03-31 15:55:20 -0400

    set up some styles to buttons

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -25,22 +25,22 @@ export default function Profile() {
             {
                 // If for some reason user is null show loading screen
                 user ? 
-                <div className={style.container}>
+                <div className={style.container} >
                     <h1 className='bg-secondary'>My profile</h1>
                     {/* Remove this later */}
-                    <div className={style.wrapper}>
+                    <div className={style.wrapper} >
                         <img className={style.img}
                             src={user ? imageHandler(user.image) : userIcon} 
                             alt="My profile image" />
                         <h2>{user?.username}</h2>
                         <button
-                        className={style.button} 
+                        className={`${style.button}`} 
                         onClick={() => showHandler("password")}>
                             Change password</button>
                         <button 
-                        className={style.button} 
+                        className={`${style.button} l-top-margin`} 
                         onClick={() => showHandler("image")}>Change image</button>
-                        <PasswordForm  show={show} close={closeHander}/>
+                        <PasswordForm show={show} close={closeHander}/>
                         <ImageForm show={show} close={closeHander} />
                     </div>
                 </div>
