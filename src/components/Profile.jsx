b5b0d1bca318f67e9830d13f51e40932647f4000
Neftaly <neftalyrv23@gmail.com>
2024-03-31 12:09:31 -0400

    Styled the proofile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,6 +3,7 @@ import userIcon from '../assets/user.svg'
 import PasswordForm from './PasswordForm'
 import ImageForm from './ImageForm'
 import { UserContext } from '../contexts/UserContext'
+import style from '../stylesheets/profile.module.css'
 export default function Profile() {
     const { user } = useContext(UserContext)
     const [show, setShow] = useState(null)
@@ -24,22 +25,24 @@ export default function Profile() {
             {
                 // If for some reason user is null show loading screen
                 user ? 
-                <div>
-                    <h3>My profile</h3>
+                <div className={style.container}>
+                    <h1 className='bg-secondary'>My profile</h1>
                     {/* Remove this later */}
-                    <img style={{
-                        width: '200px',
-                        height: '200px'
-                    }}
-                        src={user ? imageHandler(user.image) : userIcon} 
-                        alt="My profile image" />
-                    <h3>{user?.username}</h3>
-                    <button 
-                    onClick={() => showHandler("password")}>
-                        Change password</button>
-                    <button onClick={() => showHandler("image")}>Change profile image</button>
-                    <PasswordForm  show={show} close={closeHander}/>
-                    <ImageForm show={show} close={closeHander} />
+                    <div className={style.wrapper}>
+                        <img className={style.img}
+                            src={user ? imageHandler(user.image) : userIcon} 
+                            alt="My profile image" />
+                        <h2>{user?.username}</h2>
+                        <button
+                        className={style.button} 
+                        onClick={() => showHandler("password")}>
+                            Change password</button>
+                        <button 
+                        className={style.button} 
+                        onClick={() => showHandler("image")}>Change image</button>
+                        <PasswordForm  show={show} close={closeHander}/>
+                        <ImageForm show={show} close={closeHander} />
+                    </div>
                 </div>
                 : <div>Loading...</div>
             }
