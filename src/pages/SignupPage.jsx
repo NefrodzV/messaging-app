e75afcc4362d09a25bce167474bba40051e64991
Neftaly <neftalyrv23@gmail.com>
2024-04-02 20:19:30 -0400

    Implemented logo

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -4,7 +4,7 @@ import signUpStyles from '../stylesheets/signup.module.css'
 export default function SignUpPage() {
     return(
         <div className={signUpStyles.page}>
-            <div className="brand">LOGO</div>
+            <div className="logo-primary">MSGR</div>
             <SignupForm />
         </div>
     )
