f5312e6c89dc2dceab26517d13c3887231f3ddc6
Neftaly <neftalyrv23@gmail.com>
2024-04-03 14:29:00 -0400

    Notification component

---
+++ src/components/Notification.jsx
@@ -0,0 +1,25 @@
+import { memo, useEffect, useRef, useState } from "react";
+const Notification = memo(function Notification({ text, notify }) {
+
+    const notificationRef = useRef()
+    useEffect(() => {
+        if(!notify) return
+        
+        const notification = notificationRef.current
+        notification.setAttribute('show', true)
+        const id = setTimeout(() => {
+            notification.removeAttribute('show')
+        },2000)
+
+        return () => {
+            clearTimeout(id)
+        }
+    }, [notify])
+    return (
+        <div ref={notificationRef} className="notification m-top-margin" >
+            <p>{text}</p>
+        </div>
+    )
+})
+export default Notification
+
