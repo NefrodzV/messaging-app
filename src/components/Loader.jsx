b1834844f397f93f8ffa3340ed884f3dbda12755
Neftaly <neftalyrv23@gmail.com>
2024-04-03 20:23:11 -0400

    Added some inline style for loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,14 @@
 import style from '../stylesheets/loader.module.css'
 // NOTE: Right now we are representing 
 // this as a full page with the loader in the middle
-export default function Loader() {
+export default function Loader({  width, height, marginTop }) {
     return (
         <div className={style.wrapper}>
-            <div className={style.loader}>
-
+            <div className={style.loader} style={{
+                width: width,
+                height: height,
+                marginTop:marginTop
+            }}>
             </div>
         </div>
     )
