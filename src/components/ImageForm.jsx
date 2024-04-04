a3ed644d5a709f60336190a26d698d52d14f8ce2
Neftaly <neftalyrv23@gmail.com>
2024-04-03 20:24:20 -0400

    Added a loader and disable the buttons when sending the data

--- src/components/ImageForm.jsx
+++ src/components/ImageForm.jsx
@@ -2,30 +2,45 @@ import useUpdateImage from "../hooks/useUpdateImage";
 import Input from "./Input";
 import Error from "../components/Error"
 import style from '../stylesheets/dialog.module.css'
-
+import Notification from  '../components/Notification'
+import { useEffect } from "react";
+import Loader from "./Loader";
 export default function ImageForm({ show, close }) {
-    const { status, upload, errors } = useUpdateImage()
+    const { status, upload, errors, reset } = useUpdateImage()
+
+    useEffect(() => {
+        if(status === 'success') reset()
+        console.log('status changed')
+    },[status])
 
     function submitHandler(e) {
         e.preventDefault()
-        // console.log(e.target["image"].files[0])
         upload(e)
     }
     return(
         <dialog open={show === "image"} >
-            <form onSubmit={submitHandler} encType="multipart/form-data">
+            <form className={style.content}onSubmit={submitHandler} encType="multipart/form-data">
                 <h2>Select new image: </h2>
-                <button type="button" onClick={close}>Close</button>
-                <div className="group">
+               
+                <div className="group bg-top-margin">
                     <Input type={"file"} name={"image"} />
                     <Error name={"image"} errors={errors} />
                 </div>
-            <button className="bg-top-margin">submit</button>
-            <button type='button' onClick={close} style={
-                    {
-                        marginLeft: 8
-                    }
-                }>cancel</button>
+                <button 
+                    disabled={status === 'pending'}
+                    className="bg-top-margin">submit
+                </button>
+                <button  
+                    disabled={status === 'pending'}
+                    type='button' onClick={close} style={
+                        {
+                            marginLeft: 8
+                        }
+                    }>cancel</button>
+            {status === 'pending' ? <Loader  width={10} height={10} marginTop={12}/> : null }
+            <Notification 
+                text={'Profile image updated!'} 
+                notify={status === 'success'} />
             </form>
         </dialog>
     )
