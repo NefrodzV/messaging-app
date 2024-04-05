a7050c54f70feb0a5aedcad1171a215f1bf65c1a
Neftaly <neftalyrv23@gmail.com>
2024-04-04 20:27:15 -0400

    making a prop for loader wrapper height

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,13 @@
 import style from '../stylesheets/loader.module.css'
 // NOTE: Right now we are representing 
 // this as a full page with the loader in the middle
-export default function Loader({  width, height, marginTop }) {
+export default function Loader({  width, height, marginTop, containerHeight }) {
     return (
-        <div className={style.wrapper}>
+        <div className={style.wrapper} style={
+            {
+                height: containerHeight
+            }
+        }>
             <div className={style.loader} style={{
                 width: width,
                 height: height,
