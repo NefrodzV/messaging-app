adcbcceda4f733985d9c9f2cc65e834abae6828b
Neftaly <neftalyrv23@gmail.com>
2024-04-11 16:48:24 -0400

    made the correct conditions to handle undefined and null

--- src/hooks/useUtils.jsx
+++ src/hooks/useUtils.jsx
@@ -3,11 +3,9 @@ import userIcon from "../assets/user.svg"
 export default function useUtils() {
     function imageHandler(image) {
         if(!image || image === undefined) return userIcon
-        for(const [key, value] of Object.entries(image)) {
-            if(!key || key === undefined) return userIcon
-            if(!value || value === undefined) return userIcon
-        }  
-         
+        if(image.mimeType === undefined || !image.mimeType) return userIcon
+        if(image.binData === undefined || !image.binData) return userIcon
+        if(!image.binData.data || image.binData.data === undefined) return userIcon
         const buff = Buffer.from(image.binData.data)
         const url = `data:${image.mimeType};base64,${buff.toString('base64')}`
         return url
