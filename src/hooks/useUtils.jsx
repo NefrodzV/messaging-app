71b15a9b1c7e96b0c6dbe571f731455841ef5b52
Neftaly <neftalyrv23@gmail.com>
2024-04-11 11:14:16 -0400

    Made the condition flags more concise

--- src/hooks/useUtils.jsx
+++ src/hooks/useUtils.jsx
@@ -2,7 +2,12 @@ import { Buffer } from "buffer"
 import userIcon from "../assets/user.svg"
 export default function useUtils() {
     function imageHandler(image) {
-        if(!image || image === undefined || image.binData === undefined) return userIcon
+        if(!image || image === undefined) return userIcon
+        for(const [key, value] of Object.entries(image)) {
+            if(!key || key === undefined) return userIcon
+            if(!value || value === undefined) return userIcon
+        }  
+         
         const buff = Buffer.from(image.binData.data)
         const url = `data:${image.mimeType};base64,${buff.toString('base64')}`
         return url
