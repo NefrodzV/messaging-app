62dfc6552735ce64430ee19bdbdb5ce4701c31cb
Neftaly <neftalyrv23@gmail.com>
2024-04-10 22:09:11 -0400

    removed some unnecesarry code

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import router from './Router'
 import  { UserContext }  from './contexts/UserContext'
 import { useState, useEffect } from 'react'
 import Cookies from 'js-cookie'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient()
 function App() {
 
   const [token, setToken] = useState(null)
