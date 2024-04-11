e5e6e1010d82077148b34fe64216fc2916711826
Neftaly <neftalyrv23@gmail.com>
2024-04-10 21:20:35 -0400

    Made some checks for the image

--- src/hooks/useUtils.jsx
+++ src/hooks/useUtils.jsx
@@ -1,6 +1,8 @@
 import { Buffer } from "buffer"
+import userIcon from "../assets/user.svg"
 export default function useUtils() {
     function imageHandler(image) {
+        if(image.binData === undefined || !image || image === undefined) return userIcon
         const buff = Buffer.from(image.binData.data)
         const url = `data:${image.mimeType};base64,${buff.toString('base64')}`
         return url
