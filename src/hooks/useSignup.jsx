ca0a384ff0c213c18159dac575bb219ca756970a
Neftaly <neftalyrv23@gmail.com>
2024-08-24 15:55:31 -0400

    Fixed some issue with the code not using env vars with api url and incorrect use of a non value in state it should be null

--- src/hooks/useSignup.jsx
+++ src/hooks/useSignup.jsx
@@ -1,45 +1,36 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-export default function  useSignup() {
-
-    const [errors, setErrors] = useState({})
-    const [status, setStatus] = useState(null)
+export default function useSignup() {
+    const [errors, setErrors] = useState(null);
+    const [status, setStatus] = useState(null);
 
     async function signup(data) {
         try {
-            setStatus('pending')
-            const request = await fetch(
-            'https://messaging-api.adaptable.app/api/session/register',
-            {
+            setStatus('pending');
+            const request = await fetch(import.meta.env.VITE_API, {
                 method: 'POST',
                 headers: {
-                    "Content-Type" : "application/json"
+                    'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(data),
                 mode: 'cors',
-                credentials: 'same-origin'
+                // credentials: 'same-origin',
+            });
 
-                
-            })
+            const response = await request.json();
 
-            const response = await request.json()
-            
-            if(!request.ok) {
-                setErrors(response.errors)
-                setStatus('error')
-                return
+            if (!request.ok) {
+                setErrors(response.errors);
+                setStatus('error');
+                return;
             }
-            setStatus('success')
-            setErrors({})
-
-        } catch(e) {
-            setStatus('error')
-            console.log("Something went with signup" + e)
-        } 
+            setStatus('success');
+            setErrors(null);
+        } catch (e) {
+            setStatus('error');
+            console.error('Something went with signup:' + e);
+        }
     }
 
-    function reset() {
-        setStatus(null)
-    }
-    return { signup, reset, status, errors }
+    return { signup, status, errors };
 }
