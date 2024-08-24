8e94a9a4455d3718576f83ee78688a3324827768
Neftaly <neftalyrv23@gmail.com>
2024-08-23 21:15:44 -0400

    Changed the element of login page and renamed a file

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,43 +1,31 @@
-import style from '../stylesheets/Login.module.css'
-import LoginForm from '../components/LoginForm'
-import { useContext, useEffect, useState} from 'react'
-import { useNavigate } from 'react-router-dom'
-import { UserContext } from '../contexts/UserContext'
-import Loader from '../components/Loader'
+import style from '../stylesheets/Login.module.css';
+import LoginForm from '../components/LoginForm';
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../contexts/UserContext';
 
 export default function LoginPage() {
-    const navigate = useNavigate()
-    const { isLoggedIn, user } = useContext(UserContext)
-    const [loading, setLoading] = useState(true)
-    
+    const navigate = useNavigate();
+    const { isLoggedIn, user } = useContext(UserContext);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
-        if(isLoggedIn && user)  {
-            navigate('/chats')
-            return
-        } else if(isLoggedIn) {
-            setLoading(true)
+        if (isLoggedIn && user) {
+            navigate('/chats');
+            return;
+        } else if (isLoggedIn) {
+            setLoading(true);
         } else {
-            setLoading(false)
+            setLoading(false);
         }
-    },[isLoggedIn, user])
+    }, [isLoggedIn, user]);
 
     return (
         <main className={style.page}>
-            <img className={style.hero} src="" alt="" />
-
+            {/* <img className={style.hero} src="" alt="" /> */}
             <div className={style.container}>
-               <LoginForm />
+                <LoginForm />
             </div>
-            
         </main>
-        // <>
-        //     <div className={loginStyles.page}>
-        //             <div className='logo-primary'>MSGR</div>
-        //             { loading ? 
-        //                 <Loader containerHeight={'auto'}  /> : 
-        //                 <LoginForm />
-        //             }
-        //     </div>
-        // </> 
-    )
+    );
 }
