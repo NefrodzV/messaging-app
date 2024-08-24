222a1954cf3d2eb933ca9eb441798df094ce2949
Neftaly <neftalyrv23@gmail.com>
2024-08-23 21:18:01 -0400

    refactor the component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,19 +1,10 @@
-import style from '../stylesheets/loader.module.css'
-// NOTE: Right now we are representing 
+import style from '../stylesheets/loader.module.css';
+// NOTE: Right now we are representing
 // this as a full page with the loader in the middle
-export default function Loader({  width, height, marginTop, containerHeight }) {
+export default function Loader({ width, height, marginTop, containerHeight }) {
     return (
-        <div className={style.wrapper} style={
-            {
-                height: containerHeight
-            }
-        }>
-            <div className={style.loader} style={{
-                width: width,
-                height: height,
-                marginTop:marginTop
-            }}>
-            </div>
+        <div className={style.wrapper}>
+            <div className={style.loader}></div>
         </div>
-    )
+    );
 }
