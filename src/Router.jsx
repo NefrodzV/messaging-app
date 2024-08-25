3543159e603a7f3e375f6783bc34f969ea709dc3
Neftaly <neftalyrv23@gmail.com>
2024-08-25 16:14:32 -0400

    Added Navigate to go to login

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,56 +1,57 @@
-import  { 
-    createBrowserRouter,
-} from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom';
 
-import SignupPage from './pages/SignupPage'
-import LoginPage from './pages/LoginPage'
-import HomePage from './pages/HomePage'
-import UserList from './components/UserList'
-import ChatList from './components/ChatList'
-import Chat from './components/Chat'
-import CreateChat from './components/CreateChat'
-import Profile from './components/Profile'
-
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+import UserList from './components/UserList';
+import ChatList from './components/ChatList';
+import Chat from './components/Chat';
+import CreateChat from './components/CreateChat';
+import Profile from './components/Profile';
+import { Navigate } from 'react-router-dom';
 const router = createBrowserRouter([
     {
         path: '/',
+        element: <Navigate to="/login" replace={true} />,
+    },
+    {
+        path: '/:username',
         element: <HomePage />,
         children: [
             {
-                index:true,
-                path:'chats',
-                element: <ChatList/>
+                index: true,
+                path: 'chats',
+                element: <ChatList />,
             },
             {
-                path: "chats/:id",
-                element: <Chat />, 
+                path: 'chats/:id',
+                element: <Chat />,
             },
             {
-                path:'chats/create',
-                element: <Chat/>
+                path: 'chats/create',
+                element: <Chat />,
             },
             {
-                path:'users',
-                element: <UserList />
+                path: 'users',
+                element: <UserList />,
             },
-             
+
             {
                 path: 'profile',
-                element: <Profile />
-            }
-        ]
+                element: <Profile />,
+            },
+        ],
     },
     {
         path: '/login',
         element: <LoginPage />,
-        index: true
+        index: true,
     },
-    
-    {
-        path: "/signup",
-        element: <SignupPage />
-    },
-    
-])
 
-export default router
+    {
+        path: '/signup',
+        element: <SignupPage />,
+    },
+]);
+
+export default router;
