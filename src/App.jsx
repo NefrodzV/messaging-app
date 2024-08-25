69af0f515a474b879a9ec63a0aafefbdb230acab
Neftaly <neftalyrv23@gmail.com>
2024-08-25 16:13:05 -0400

    uploading the chat item component and some svgs. Update the app class to use the UserProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,58 @@
-import { RouterProvider } from 'react-router-dom'
-import router from './Router'
-import  { UserContext }  from './contexts/UserContext'
-import { useState, useEffect } from 'react'
-import Cookies from 'js-cookie'
-
+import { RouterProvider } from 'react-router-dom';
+import router from './Router';
+import { UserContext } from './contexts/UserContext';
+import { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
+import UserProvider from './providers/UserProvider';
 function App() {
+    const [token, setToken] = useState(null);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState(null);
 
-  const [token, setToken] = useState(null)
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [user, setUser] = useState(null)
-
-  useEffect(() => {
-    const data = Cookies.get('token')
-    setToken(data)
-  })
-  useEffect(() => {
-    if(token) {
-      // Cookies.set('token', token, { expires: 1 })
-      setIsLoggedIn(true)
-      getUser()
-    } else {
-      setIsLoggedIn(false)
-    }
-  },[token])
-
-  async function getUser() {
-    try {
-      const response = await fetch(
-        'https://messaging-api.adaptable.app/api/users/me',
-        {
-          headers: {
-            'authorization': 'Bearer ' + token
-          },
-          mode: 'cors',
-          credentials: 'same-origin'
+    useEffect(() => {
+        const data = Cookies.get('token');
+        setToken(data);
+    });
+    useEffect(() => {
+        if (token) {
+            // Cookies.set('token', token, { expires: 1 })
+            setIsLoggedIn(true);
+            getUser();
+        } else {
+            setIsLoggedIn(false);
         }
-      )
-        const data = await response.json()
-        if(!response.ok) {
-            console.log(data)
-            return console.error("error getting user", data.errors)
-        }
-        setUser(data.user)
-    } catch(e) {
-        throw new Error("Error getting user from api: " + e)
-    }
-  }
+    }, [token]);
 
-  const updateUser = () => getUser()
-  
-  return (
-    <>
-      <UserContext.Provider value={{
-        token, 
-        user,
-        updateUser,
-        isLoggedIn,
-        setToken,
-        setIsLoggedIn, 
-        }} >
-          <RouterProvider router={router} />
-      </UserContext.Provider>
-    </>
-  )
+    async function getUser() {
+        try {
+            const response = await fetch(
+                'https://messaging-api.adaptable.app/api/users/me',
+                {
+                    headers: {
+                        authorization: 'Bearer ' + token,
+                    },
+                    mode: 'cors',
+                    credentials: 'same-origin',
+                }
+            );
+            const data = await response.json();
+            if (!response.ok) {
+                console.log(data);
+                return console.error('error getting user', data.errors);
+            }
+            setUser(data.user);
+        } catch (e) {
+            throw new Error('Error getting user from api: ' + e);
+        }
+    }
+
+    const updateUser = () => getUser();
+
+    return (
+        <UserProvider>
+            <RouterProvider router={router} />{' '}
+        </UserProvider>
+    );
 }
 
-export default App
+export default App;
