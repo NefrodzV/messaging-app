681a778e4224c57fcb2e838f600aaeb1490adb4d
Neftaly <neftalyrv23@gmail.com>
2024-08-31 18:16:26 -0400

    correctly got the context data

--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -1,39 +1,39 @@
 import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../contexts/UserContext';
+
 export default function useUser() {
-    const { user, setUser } = UserContext;
+    const { user, setUser } = useContext(UserContext);
 
-    async function getUser() {
-        try {
-            const response = await fetch(
-                'https://messaging-api.adaptable.app/api/users/me',
-                {
-                    headers: {
-                        authorization: 'Bearer ' + token,
-                    },
-                    mode: 'cors',
-                }
-            );
-
-            const data = await response.json();
-            if (!response.ok) {
-                return console.error('error getting user', data.errors);
-            }
-            setUser(data.user);
-        } catch (e) {
-            throw new Error('Error getting user from api: ' + e);
-        } finally {
-            setLoading(false);
-        }
-    }
-
-    useEffect(() => {
-        getUser();
-    }, []);
+    //     async function getUser() {
+    //         try {
+    //             const response = await fetch(
+    //                 'https://messaging-api.adaptable.app/api/users/me',
+    //                 {
+    //                     headers: {
+    //                         authorization: 'Bearer ' + token,
+    //                     },
+    //                     mode: 'cors',
+    //                 }
+    //             );
+    //
+    //             const data = await response.json();
+    //             if (!response.ok) {
+    //                 return console.error('error getting user', data.errors);
+    //             }
+    //             setUser(data.user);
+    //         } catch (e) {
+    //             throw new Error('Error getting user from api: ' + e);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     }
+    //
+    //     useEffect(() => {
+    //         getUser();
+    //     }, []);
 
     return {
         user,
         setUser,
-        loading,
     };
 }
