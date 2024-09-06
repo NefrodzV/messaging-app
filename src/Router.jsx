84353cf6d95566df07ac387e85d7306634e8d705
Neftaly <neftalyrv23@gmail.com>
2024-09-06 18:54:58 -0400

    removed unused components imports

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
-
 import SignupPage from './pages/SignupPage';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
-import UserList from './components/UserList';
-import ChatList from './components/ChatList';
 import Chat from './components/Chat';
-import CreateChat from './components/CreateChat';
-import Profile from './components/Profile';
 import { Navigate } from 'react-router-dom';
 const router = createBrowserRouter([
     {
