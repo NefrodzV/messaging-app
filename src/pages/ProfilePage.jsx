9e8a1270e596f3072a89561eda46d03d8ab1b206
Neftaly <neftalyrv23@gmail.com>
2024-09-07 20:21:15 -0400

    Made the profile page and that the user can select their profile images and select it

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,4 +1,56 @@
 import style from '../stylesheets/ProfilePage.module.css';
+import Header from '../components/Header';
+import ChatList from '../components/ChatList';
+import userSvg from '../assets/svgs/user.svg';
+import useMediaQuery from '../hooks/useMediaQuery';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 export default function ProfilePage() {
-    return <div className="page">My profile</div>;
+    const { queryIsActive } = useMediaQuery('(max-width: 768px)');
+    const [userImage, setUserImage] = useState('');
+    return (
+        <div className={style.page}>
+            <Header />
+            <main className={style.main}>
+                {!queryIsActive && <ChatList />}
+                <section className={style.profile}>
+                    <div className={style.hero}></div>
+
+                    <article className={style.user}>
+                        <h1>My profile</h1>
+
+                        <img
+                            className={style.userImg}
+                            src={userImage || userSvg}
+                            alt="User profile image"
+                        />
+
+                        <h2>Rose Vargas Hernandez</h2>
+                        <label className={style.imgLabel} htmlFor="image">
+                            update image
+                        </label>
+                        <input
+                            onLoad={() => {
+                                console.log('load has been called');
+                            }}
+                            onChange={(e) => {
+                                const file = e.target.files[0];
+                                const reader = new FileReader();
+                                reader.readAsDataURL(file);
+                                reader.addEventListener('load', () => {
+                                    console.log('a load has been perfomed');
+                                    console.log(reader.result);
+                                    setUserImage(reader.result);
+                                });
+                            }}
+                            id="image"
+                            type="file"
+                            className={style.imgInput}
+                        ></input>
+                        <Link>Sign Out</Link>
+                    </article>
+                </section>
+            </main>
+        </div>
+    );
 }
