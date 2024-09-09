46dae3fece0c91aceb2e4813dd54aadac9a671bd
Neftaly <neftalyrv23@gmail.com>
2024-09-09 16:34:19 -0400

    Made the component for the profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,46 @@
+import style from '../stylesheets/ProfilePage.module.css';
+import userSvg from '../assets/svgs/user.svg';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+export default function Profile() {
+    const [userImage, setUserImage] = useState('');
+    return (
+        <section className={style.profile}>
+            <div className={style.hero}></div>
+
+            <article className={style.user}>
+                <h1>My profile</h1>
+
+                <img
+                    className={style.userImg}
+                    src={userImage || userSvg}
+                    alt="User profile image"
+                />
+
+                <h2>Rose Vargas Hernandez</h2>
+                <label className={style.imgLabel} htmlFor="image">
+                    update image
+                </label>
+                <input
+                    onLoad={() => {
+                        console.log('load has been called');
+                    }}
+                    onChange={(e) => {
+                        const file = e.target.files[0];
+                        const reader = new FileReader();
+                        reader.readAsDataURL(file);
+                        reader.addEventListener('load', () => {
+                            console.log('a load has been perfomed');
+                            console.log(reader.result);
+                            setUserImage(reader.result);
+                        });
+                    }}
+                    id="image"
+                    type="file"
+                    className={style.imgInput}
+                ></input>
+                <Link>Sign Out</Link>
+            </article>
+        </section>
+    );
+}
