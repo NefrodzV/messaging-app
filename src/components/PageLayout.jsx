eeb36e2884e3c6bdb019e7432accfeef921e5e26
Neftaly <neftalyrv23@gmail.com>
2024-09-09 16:49:39 -0400

    Made it so that if the user nabigates from another page and it doesnt re-render the useEffect calculates if it should navigate.

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,9 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useParams, useNavigate, useLocation } from 'react-router-dom';
 import Header from './Header';
 import useMediaQuery from '../hooks/useMediaQuery';
 import style from '../stylesheets/PageLayout.module.css';
+import { useEffect } from 'react';
 export default function PageLayout() {
-    const { queryIsActive } = useMediaQuery('(max-width: 768px)');
+    const { queryIsActive } = useMediaQuery('(max-width:768px)');
+    const { chatId } = useParams();
+    const navigate = useNavigate();
+    const { pathname } = useLocation();
+    useEffect(() => {
+        if (!pathname.includes('chats')) return;
+        if (!queryIsActive && !chatId) {
+            navigate('/chats/lastChat');
+        }
+    }, [queryIsActive, pathname]);
     return (
         <div className={style.layout}>
             <Header />
