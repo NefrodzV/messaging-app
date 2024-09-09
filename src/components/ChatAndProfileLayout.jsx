f5458481820055602a13b7cf82b92c220e39b9ab
Neftaly <neftalyrv23@gmail.com>
2024-09-09 16:53:37 -0400

    Update that chatlist only show on certain conditions like if the device is a larger screen size and if id is not present

--- src/components/ChatAndProfileLayout.jsx
+++ src/components/ChatAndProfileLayout.jsx
@@ -1,10 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useParams } from 'react-router-dom';
 import ChatList from './ChatList';
-
+import useMediaQuery from '../hooks/useMediaQuery';
 export default function ChatAndProfileLayout() {
+    const { chatId } = useParams();
+    const { queryIsActive } = useMediaQuery('(max-width:768px)');
+    const { pathname } = useLocation();
     return (
         <>
-            <ChatList />
+            {/** Only show the chatlist if query is not active. If the query
+             * is active and the pathname is /chats only show chatlist if there
+             * chat id
+             */}
+            {!queryIsActive ? (
+                <ChatList />
+            ) : (
+                pathname.includes('/chats') && !chatId && <ChatList />
+            )}
             <Outlet />
         </>
     );
