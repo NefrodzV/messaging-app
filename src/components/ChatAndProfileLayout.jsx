ae4e7e491c40187ad2bc3a0171d3f544fdd88428
Neftaly <neftalyrv23@gmail.com>
2024-09-10 15:35:53 -0400

    Added the scoket provider

--- src/components/ChatAndProfileLayout.jsx
+++ src/components/ChatAndProfileLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet, useLocation, useParams } from 'react-router-dom';
 import ChatList from './ChatList';
 import useMediaQuery from '../hooks/useMediaQuery';
+import SocketProvider from '../providers/SocketProvider';
 export default function ChatAndProfileLayout() {
     const { chatId } = useParams();
     const { queryIsActive } = useMediaQuery('(max-width:768px)');
     const { pathname } = useLocation();
     return (
-        <>
+        <SocketProvider>
             {/** Only show the chatlist if query is not active. If the query
              * is active and the pathname is /chats only show chatlist if there
              * chat id
@@ -17,6 +18,6 @@ export default function ChatAndProfileLayout() {
                 pathname.includes('/chats') && !chatId && <ChatList />
             )}
             <Outlet />
-        </>
+        </SocketProvider>
     );
 }
