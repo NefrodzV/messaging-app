6466273b4431f73719bbdab5ad30314a7fdbba11
Neftaly <neftalyrv23@gmail.com>
2024-09-13 14:41:37 -0400

    removed unused code

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,17 +1,7 @@
 import style from '../stylesheets/Login.module.css';
 import LoginForm from '../components/LoginForm';
-import { useContext, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import useUser from '../hooks/useUser';
 
 export default function LoginPage() {
-    const { user } = useUser();
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        console.log(user);
-        if (user) navigate('/chats/20875', { replace: true });
-    }, [user]);
     return (
         <main className={style.page}>
             {/* <img className={style.hero} src="" alt="" /> */}
