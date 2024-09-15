ed1d4659531b67f559157eaaf4d4125ddfd5707e
Neftaly <neftalyrv23@gmail.com>
2024-09-15 14:09:09 -0400

    if login is a success navigate to the homepage

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-
+import { useNavigate } from 'react-router-dom';
 export default function useLogin() {
     const [errors, setErrors] = useState(null);
     const [status, setStatus] = useState(null);
-
+    const navigate = useNavigate();
     async function login(data) {
         try {
             setStatus('pending');
@@ -19,15 +19,15 @@ export default function useLogin() {
             );
 
             const response = await request.json();
-
+            console.log(response);
             // Handle request errors
             if (!request.ok) {
                 setErrors(response.errors);
                 setStatus('error');
                 return;
             }
-            setUser(response);
             setStatus('success');
+            navigate('/', { replace: true });
         } catch (e) {
             console.error(e);
         }
