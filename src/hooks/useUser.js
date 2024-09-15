30fea2188c8fbd03ef4b939581f707d3b818669e
Neftaly <neftalyrv23@gmail.com>
2024-09-15 14:10:12 -0400

    made a hook to pass the user state in the app with the userProvider component

---
+++ src/hooks/useUser.js
@@ -0,0 +1,39 @@
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../providers/UserProvider';
+
+export default function useUser() {
+    const { user, setUser } = useContext(UserContext);
+
+    //     async function getUser() {
+    //         try {
+    //             const response = await fetch(
+    //                 'https://messaging-api.adaptable.app/api/users/me',
+    //                 {
+    //                     headers: {
+    //                         authorization: 'Bearer ' + token,
+    //                     },
+    //                     mode: 'cors',
+    //                 }
+    //             );
+    //
+    //             const data = await response.json();
+    //             if (!response.ok) {
+    //                 return console.error('error getting user', data.errors);
+    //             }
+    //             setUser(data.user);
+    //         } catch (e) {
+    //             throw new Error('Error getting user from api: ' + e);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     }
+    //
+    //     useEffect(() => {
+    //         getUser();
+    //     }, []);
+
+    return {
+        user,
+        setUser,
+    };
+}
