4aafe525b6e01eeaff090f154e199ccaef0af6a5
Neftaly <neftalyrv23@gmail.com>
2024-09-18 12:15:32 -0400

    chats navlink only visible in smaller screen sizes

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -14,34 +14,33 @@ export default function Navigation() {
     const { queryIsActive } = useMediaQuery('(max-width: 768px)');
     const { chatId } = useParams();
     const handleNavLinkActive = ({ isActive, isPending, isTransitioning }) => {
-        console.log(isActive);
-        console.log(isPending);
-        console.log(isTransitioning);
         return isActive ? `${style.navLink} ${style.active}` : style.navLink;
     };
     return (
         <nav className={style.navList}>
             <div className={style.logo}>Logo</div>
-            <NavLink
-                className={handleNavLinkActive}
-                title="Go to my chats"
-                aria-label="Go to my chats"
-                to={'chats'}
-            >
-                <div className={style.content}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 512 512"
-                    >
-                        <path
-                            fill="currentColor"
-                            d="M64 0C28.7 0 0 28.7 0 64L0 352c0 35.3 28.7 64 64 64l96 0 0 80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416 448 416c35.3 0 64-28.7 64-64l0-288c0-35.3-28.7-64-64-64L64 0z"
-                        />
-                    </svg>
+            {queryIsActive && (
+                <NavLink
+                    className={handleNavLinkActive}
+                    title="Go to my chats"
+                    aria-label="Go to my chats"
+                    to={'chats'}
+                >
+                    <div className={style.content}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                        >
+                            <path
+                                fill="currentColor"
+                                d="M64 0C28.7 0 0 28.7 0 64L0 352c0 35.3 28.7 64 64 64l96 0 0 80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416 448 416c35.3 0 64-28.7 64-64l0-288c0-35.3-28.7-64-64-64L64 0z"
+                            />
+                        </svg>
 
-                    <span className={style.text}>Chats</span>
-                </div>
-            </NavLink>
+                        <span className={style.text}>Chats</span>
+                    </div>
+                </NavLink>
+            )}
             <NavLink
                 className={handleNavLinkActive}
                 title="Go to my profile"
