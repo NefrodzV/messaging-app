de2335e1b2178c8128e04bf2a324f79996177a77
Neftaly <neftalyrv23@gmail.com>
2024-09-21 11:04:19 -0400

    made index file for hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,5 @@
+export { default as useChat } from './useChat';
+export { default as useSocket } from './useSocket';
+export { default as useSignup } from './useSignup';
+export { default as useUsers } from './useUsers';
+export { default as useUser } from './useUser';
