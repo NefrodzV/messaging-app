98f1c9b4477d82f0a650e040e1d80330e6f823a1
Neftaly <neftalyrv23@gmail.com>
2024-09-21 15:54:52 -0400

    ficed issue user image not showing and formatting the date to a string

--- src/components/MessageItem.jsx
+++ src/components/MessageItem.jsx
@@ -1,20 +1,23 @@
 import style from '../stylesheets/MessageItem.module.css';
 import userSvg from '../assets/svgs/user.svg';
 export default function MessageItem({ message, onClick }) {
-    const { _id, user, text, date, imgs } = message;
-
+    const { _id, user, text, date, imgs, mine } = message;
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        return date.toLocaleString();
+    }
     return (
-        <article className={style.message}>
+        <article className={style.message} data-reverse={mine}>
             <div
                 className={style.bubble}
                 onClick={onClick.bind('message', message)}
             >
                 <p className={style.text}>{text}</p>
-                <span className={style.time}>8:12 am</span>
+                <span className={style.time}>{formatDate(date)}</span>
             </div>
             <img
                 className={style.userImage}
-                src={user?.imageUrl || userSvg}
+                src={user?.image || userSvg}
                 alt={`${user?.username} profile image`}
             />
         </article>
