f0d3be2f44e8f2a325fda143ca467b50e6b6d4eb
Neftaly <neftalyrv23@gmail.com>
2024-09-23 18:55:49 -0400

    Made a toast component to show messages

---
+++ src/components/Toast.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import style from '../stylesheets/Toast.module.css';
+import errorSvg from '../assets/svgs/exclamation.svg';
+
+const icons = {
+    error: errorSvg,
+};
+export default function Toast({ message, type }) {
+    const [isMounted, setIsMounted] = useState(true);
+    const [isActive, setIsActive] = useState(false);
+    useEffect(() => {
+        if (message) setIsActive(true);
+    }, [message]);
+
+    useEffect(() => {
+        if (isActive) {
+            setTimeout(() => {
+                setIsActive(false);
+            }, 2000);
+        }
+    }, [isActive]);
+    return (
+        <>
+            {isMounted && (
+                <div
+                    className={`${style.toast} ${style[type]}`}
+                    data-active={isActive}
+                    onAnimationEnd={() => {
+                        if (!isActive && isMounted) setIsMounted(false);
+                    }}
+                >
+                    <span className={style.iconContainer}>
+                        <img src={icons[type]} />
+                    </span>
+                    <span>{message}</span>
+                </div>
+            )}
+        </>
+    );
+}
