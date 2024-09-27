3f7cfad69c89aa21945cd83615781e9367c6c940
Neftaly <neftalyrv23@gmail.com>
2024-09-26 20:56:08 -0400

    removed use effect for some reason it continues loading and onload doesnt fire again

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -3,9 +3,6 @@ import { Loader } from '.';
 export default function Image({ url, className, alt }) {
     const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        if (url) setIsLoading(true);
-    }, [url]);
     return (
         // Needs position relative so loader can cover the image
         <div style={{ position: 'relative' }}>
