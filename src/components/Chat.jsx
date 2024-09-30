e8e721b01bf822519690268761f51bf1059161ea
Neftaly <neftalyrv23@gmail.com>
2024-09-30 16:59:50 -0400

    added an input to upload files

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -106,6 +106,26 @@ export default function Chat() {
                             }}
                         />
                     </div>
+                    <label
+                        htmlFor="images"
+                        className={style.button}
+                        title="Upload files"
+                        aria-label="Click here to upload files"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                            fill="currentColor"
+                        >
+                            <path d="M288 109.3L288 352c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-242.7-73.4 73.4c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l128-128c12.5-12.5 32.8-12.5 45.3 0l128 128c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L288 109.3zM64 352l128 0c0 35.3 28.7 64 64 64s64-28.7 64-64l128 0c35.3 0 64 28.7 64 64l0 32c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64l0-32c0-35.3 28.7-64 64-64zM432 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z" />
+                        </svg>
+                        <input
+                            className={style.imgsInput}
+                            name="images"
+                            type="file"
+                            id="images"
+                        />
+                    </label>
                     <button
                         className={style.sendButton}
                         title="Send message button"
