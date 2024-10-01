caa40db91dcb7686ba0d56e0530a45cc236838be
Neftaly <neftalyrv23@gmail.com>
2024-10-01 19:36:53 -0400

    changed the element to section added a header and a close button to remove all files if pressed

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -15,7 +15,13 @@ import FormImage from './FormImage';
 
 export default function Chat() {
     const [text, setText] = useState('');
-    const [images, setImages] = useState([dummyPic1]);
+    const [images, setImages] = useState([
+        dummyPic1,
+        // dummyPic1,
+        // dummyPic1,
+        // dummyPic1,
+        // dummyPic1,
+    ]);
     const [isDialogOpen, setIsDialogOpen] = useState(false);
     const { chat, error, updateMessage, deleteMessage, sendMessage } =
         useChat();
@@ -76,16 +82,34 @@ export default function Chat() {
                     });
                 }}
             >
-                <div className={style.imageContainer}>
-                    <button style={{ float: 'right' }}>Close</button>
-                    {images?.map((image, i) => (
-                        <FormImage
-                            key={i}
-                            url={image}
-                            className={style.formImage}
-                        />
-                    ))}
-                </div>
+                <section className={style.files}>
+                    <header>
+                        <h2>Files</h2>
+                        <div className="flex-container">
+                            <button
+                                title="Close and remove all files selected"
+                                aria-label="Close and remove all files selected"
+                                className="primary wrap-container only-svg red"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 384 512"
+                                >
+                                    <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+                                </svg>
+                            </button>
+                        </div>
+                    </header>
+                    <div className={style.fileContainer}>
+                        {images?.map((image, i) => (
+                            <FormImage
+                                key={i}
+                                url={image}
+                                className={style.formImage}
+                            />
+                        ))}
+                    </div>
+                </section>
                 <div className={style.container}>
                     <div className={style.wrapper}>
                         {isEditing && (
