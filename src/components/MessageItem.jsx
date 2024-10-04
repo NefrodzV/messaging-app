9360e253114391bc18f55882ad7af810d17612f5
Neftaly <neftalyrv23@gmail.com>
2024-10-04 13:59:10 -0400

    added images to messages

--- src/components/MessageItem.jsx
+++ src/components/MessageItem.jsx
@@ -2,9 +2,10 @@ import style from '../stylesheets/MessageItem.module.css';
 import userSvg from '../assets/svgs/user.svg';
 import { useUser } from '../hooks';
 import { formatDate } from '../utils/utils.js';
+import Image from './Image.jsx';
 export default function MessageItem({ message, onClick }) {
     const auth = useUser();
-    const { _id, user, text, date, imgs, mine } = message;
+    const { _id, user, text, date, images } = message;
 
     function messageIsFromAuthUser() {
         return auth.user.username == user.username;
@@ -20,10 +21,19 @@ export default function MessageItem({ message, onClick }) {
             >
                 <p className={style.text}>{text}</p>
                 <span className={style.time}>{formatDate(new Date(date))}</span>
+                <div className={style.images}>
+                    {images?.map((image) => (
+                        <Image
+                            className={style.image}
+                            key={image.cloudinary_public_id}
+                            url={image.url}
+                        />
+                    ))}
+                </div>
             </div>
             <img
                 className={style.userImage}
-                src={user?.image || userSvg}
+                src={user?.image.w56 || userSvg}
                 alt={`${user?.username} profile image`}
             />
         </article>
