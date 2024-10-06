849bc7b3ed4a16d59a9e3174ffa9fa779b313a3d
Neftaly <neftalyrv23@gmail.com>
2024-10-06 17:07:37 -0400

    Added error layout to root element

--- src/Router.jsx
+++ src/Router.jsx
@@ -23,6 +23,7 @@ const router = createBrowserRouter([
             return user;
         },
 
+        errorElement: <ErrorLayout />,
         children: [
             {
                 loader: () => {
