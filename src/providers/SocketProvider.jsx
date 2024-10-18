df580d3a057b551ee53b55b555274f92cbc0a931
Neftaly <neftalyrv23@gmail.com>
2024-10-18 17:51:35 -0400

    making the user join last chat

--- src/providers/SocketProvider.jsx
+++ src/providers/SocketProvider.jsx
@@ -1,10 +1,12 @@
 import { createContext, useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
+import { useUser } from '../hooks';
 const url = import.meta.env.VITE_API;
 
 export const SocketContext = createContext(null);
 
 export default function SocketProvider({ children }) {
+    const { user } = useUser();
     const [socket, setSocket] = useState(
         io(url, { withCredentials: true, transports: ['websocket'] })
     );
@@ -32,6 +34,7 @@ export default function SocketProvider({ children }) {
             ? 'socket connected'
             : 'socket disconnected';
         console.log(logMessage);
+        if (isConnected) socket?.emit('join', user?.lastChat);
     }, [isConnected]);
 
     return (
