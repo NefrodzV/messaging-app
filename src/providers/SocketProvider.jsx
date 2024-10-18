f1a65b039cc1b9b3dfd11068d49bbb3af2b1bc3f
Neftaly <neftalyrv23@gmail.com>
2024-10-18 18:07:01 -0400

    made scoket be a singleton

--- src/providers/SocketProvider.jsx
+++ src/providers/SocketProvider.jsx
@@ -2,14 +2,11 @@ import { createContext, useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 import { useUser } from '../hooks';
 const url = import.meta.env.VITE_API;
-
+const socket = io(url, { withCredentials: true, transports: ['websocket'] });
 export const SocketContext = createContext(null);
 
 export default function SocketProvider({ children }) {
     const { user } = useUser();
-    const [socket, setSocket] = useState(
-        io(url, { withCredentials: true, transports: ['websocket'] })
-    );
     const [isConnected, setIsConnected] = useState(socket?.connected);
     useEffect(() => {
         function onConnect() {
